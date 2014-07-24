d1b6f4eaa312aa6184a481fa06b464b1b3eb5f0d
Francesco Zagaria <fr488@hotmail.com>
2014-07-24 21:20:08 +0200

    modifica dipendenze e interfaccia

--- src/main/java/agroludos/presentation/views/amministratore/ControllerConfSistema.java
+++ src/main/java/agroludos/presentation/views/amministratore/ControllerConfSistema.java
@@ -12,11 +12,13 @@ import javafx.scene.layout.GridPane;
 
 public class ControllerConfSistema implements Initializable{
 	@FXML Button btnAvantiConf;
+	@FXML Button btnIndietroConf;
 	@FXML GridPane databasePane;
 	@FXML GridPane managerSistemaPane;
 	
 	public void initialize(URL arg0, ResourceBundle arg1) {
-		
+		this.databasePane.setVisible(true);
+        this.managerSistemaPane.setVisible(false);
 	}
 	
 	@FXML protected void btnAvantiClicked(MouseEvent event) {
@@ -24,4 +26,9 @@ public class ControllerConfSistema implements Initializable{
 	        this.databasePane.setVisible(false);
 	        this.managerSistemaPane.setVisible(true);
 	}
+	@FXML protected void btnIndietroClicked(MouseEvent event) {
+		System.out.println("Ciao");
+        this.databasePane.setVisible(true);
+        this.managerSistemaPane.setVisible(false);
+}
 }
