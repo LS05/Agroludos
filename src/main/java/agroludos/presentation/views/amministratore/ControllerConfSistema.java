f8c186d5f7984da5c75a0e0b379587199be21866
Francesco Zagaria <fr488@hotmail.com>
2014-07-28 18:59:38 +0200

    Controlli sui campi vuoti

--- src/main/java/agroludos/presentation/views/amministratore/ControllerConfSistema.java
+++ src/main/java/agroludos/presentation/views/amministratore/ControllerConfSistema.java
@@ -2,23 +2,40 @@ package agroludos.presentation.views.amministratore;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import agroludos.presentation.fc.FC;
 import agroludos.presentation.req.FrameRequest;
-
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.GridPane;
 
 public class ControllerConfSistema implements Initializable{
-	@FXML Button btnAvantiConf;
-	@FXML Button btnIndietroConf;
-	@FXML Button btnConfermaConfigurazione;
-	@FXML GridPane databasePane;
-	@FXML GridPane managerSistemaPane;
+	@FXML private Button btnAvantiConf;
+	@FXML private Button btnIndietroConf;
+	@FXML private Button btnConfermaConfigurazione;
+	@FXML private GridPane databasePane;
+	@FXML private GridPane managerSistemaPane;
+	
+	//texfield DataBase
+	@FXML private TextField txtPercorsoDB;
+	@FXML private TextField txtNomeDB;
+	@FXML private TextField txtUsernameDB;
+	@FXML private PasswordField txtPasswordDB;
+	
+	//texfield Manager Di Sistema
+	@FXML private TextField txtNomeMds;
+	@FXML private TextField txtCognomeMds;
+	@FXML private TextField txtUsernameMds;
+	@FXML private PasswordField txtPasswordMds;
+	@FXML private TextField txtEmailMds;
+	@FXML private TextField txtTelefonoMds;
 	
 	private FC frontController = FC.getInstance();
 	
@@ -30,17 +47,44 @@ public class ControllerConfSistema implements Initializable{
 	}
 	
 	@FXML protected void btnAvantiClicked(MouseEvent event) {
-        this.databasePane.setVisible(false);
-        this.managerSistemaPane.setVisible(true);
+		//controllo la validità delle textfield
+		if((this.txtPercorsoDB.getText().length() != 0)  && 
+			(this.txtNomeDB.getText().length() != 0)  &&
+			(this.txtUsernameDB.getText().length() != 0)  &&
+			(this.txtPasswordDB.getText().length() != 0)  
+		) {
+			this.databasePane.setVisible(false);
+	        this.managerSistemaPane.setVisible(true);
+		}
+		else {
+			System.out.println("Campi vuoti o errati");
+		}
+        
 	}
 	
 	@FXML protected void btnIndietroClicked(MouseEvent event) {
-        this.databasePane.setVisible(true);
-        this.managerSistemaPane.setVisible(false);
+		  this.databasePane.setVisible(true);
+	      this.managerSistemaPane.setVisible(false);
+		
 	}
 	
 	@FXML protected void btnConfermaConfigurazione(MouseEvent event) {
-		this.richiesta = new FrameRequest("confermaConfigurazione");
-		Object res = this.frontController.eseguiRichiesta(richiesta);
+		//controllo la validità delle textfield
+		if((this.txtNomeMds.getText().length() != 0)  && 
+			(this.txtCognomeMds.getText().length() != 0)  &&
+			(this.txtUsernameMds.getText().length() != 0)  &&
+			(this.txtPasswordMds.getText().length() != 0)  &&
+			(this.txtEmailMds.getText().length() != 0)  &&
+			(this.txtTelefonoMds.getText().length() != 0)  
+		) {
+			this.richiesta = new FrameRequest("confermaConfigurazione");
+			Object res = this.frontController.eseguiRichiesta(richiesta);
+		}
+		else {
+			System.out.println("Campi vuoti o errati");
+		}
+		
 	}
+	
+
 }
