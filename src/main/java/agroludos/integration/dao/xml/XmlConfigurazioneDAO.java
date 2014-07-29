d93f9dba8494833fa7067be8100e84c6241d3229
Francesco Zagaria <fr488@hotmail.com>
2014-07-29 18:42:53 +0200

    scrittura configurazione.xml

--- src/main/java/agroludos/integration/dao/xml/XmlConfigurazioneDAO.java
+++ src/main/java/agroludos/integration/dao/xml/XmlConfigurazioneDAO.java
@@ -1,35 +1,21 @@
 package agroludos.integration.dao.xml;
 
 import java.io.File;
-import java.io.FileReader;
 import java.io.IOException;
-import java.io.StringReader;
 
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
-import javax.xml.transform.Result;
-import javax.xml.transform.Source;
 import javax.xml.transform.Transformer;
-import javax.xml.transform.TransformerConfigurationException;
 import javax.xml.transform.TransformerException;
 import javax.xml.transform.TransformerFactory;
-import javax.xml.transform.TransformerFactoryConfigurationError;
 import javax.xml.transform.dom.DOMSource;
-import javax.xml.transform.sax.SAXSource;
 import javax.xml.transform.stream.StreamResult;
 
 import org.w3c.dom.Document;
-import org.w3c.dom.Element;
-import org.w3c.dom.NamedNodeMap;
 import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
-import org.xml.sax.Attributes;
-import org.xml.sax.InputSource;
 import org.xml.sax.SAXException;
-import org.xml.sax.XMLReader;
-import org.xml.sax.helpers.XMLFilterImpl;
-import org.xml.sax.helpers.XMLReaderFactory;
 
 import agroludos.integration.dao.ConfigurazioneDAO;
 import agroludos.to.DatabaseTO;
@@ -45,25 +31,17 @@ public class XmlConfigurazioneDAO implements ConfigurazioneDAO{
 		DocumentBuilder docBuilder = docFactory.newDocumentBuilder();
 		Document doc = docBuilder.parse(filepath);
  
-		// Get the root element
-		Node company = doc.getFirstChild();
- 
-		// Get the staff element , it may not working if tag has spaces, or
-		// whatever weird characters in front...it's better to use
-		// getElementsByTagName() to get it directly.
-		// Node staff = company.getFirstChild();
- 
-		// Get the staff element by tag name directly
+		// Get the database element by tag name directly
 		Node database = doc.getElementsByTagName("database").item(0);
  
-		// loop the staff child node
+		// loop the database child node
 		NodeList list = database.getChildNodes();
  
 		for (int i = 0; i < list.getLength(); i++) {
  
                    Node node = list.item(i);
  
-		   // get the salary element, and update the value
+		   // get the tipo element, and update the value
 		   if ("tipo".equals(node.getNodeName())) {
 			node.setTextContent("Luchein");
 		   }
@@ -74,7 +52,7 @@ public class XmlConfigurazioneDAO implements ConfigurazioneDAO{
 				node.setTextContent("pop");
 			   }
 		   if ("porta".equals(node.getNodeName())) {
-				node.setTextContent("nu ");
+				node.setTextContent("nu");
 			   }
 		   if ("username".equals(node.getNodeName())) {
 				node.setTextContent("grandessm");
