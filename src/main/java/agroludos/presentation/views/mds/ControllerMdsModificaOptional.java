f44d2c7d3279d47595a6ba2dff888a3d205e6b78
Francesco Zagaria <fr488@hotmail.com>
2014-08-20 11:15:33 +0200

    prova push

--- src/main/java/agroludos/presentation/views/mds/ControllerMdsModificaOptional.java
+++ src/main/java/agroludos/presentation/views/mds/ControllerMdsModificaOptional.java
@@ -1,5 +1,17 @@
 package agroludos.presentation.views.mds;
 
-public class ControllerMdsModificaOptional {
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.fxml.Initializable;
+
+public class ControllerMdsModificaOptional implements Initializable{
+
+	//button 
+	@Override
+	public void initialize(URL arg0, ResourceBundle arg1) {
+		// TODO Auto-generated method stub
+		
+	}
 
 }
