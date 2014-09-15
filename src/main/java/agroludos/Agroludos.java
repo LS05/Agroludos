658723cc03b6fc9303306ff080da30e9e8c1c1d5
Luca Suriano <lucasuriano@live.it>
2014-09-15 16:38:32 +0200

    Aggiunta javadoc classe Agroludos

--- src/main/java/agroludos/Agroludos.java
+++ src/main/java/agroludos/Agroludos.java
@@ -7,6 +7,20 @@ import agroludos.main.App;
 import javafx.application.Application;
 import javafx.stage.Stage;
 
+/**
+ * La classe modella l'entry point dell'intera applicazione. La classe estende 
+ * Application ({@link javafx.application.Application}) in quanto rappresenta
+ * un'applicazione JavaFX. 
+ * Il metodo {@link start} infatti si occupa di inizializzare Spring caricando 
+ * il file xml spring.xml e di mostrare la schermata iniziale.
+ *  
+ * @author Luca Suriano
+ * @author Francesco Zagaria
+ * 
+ * @see javafx.application.Application
+ * @see org.springframework.context.support.ClassPathXmlApplicationContext#ClassPathXmlApplicationContext(String)
+ */
+
 public class Agroludos extends Application{
 	private App mainApp;
 	
