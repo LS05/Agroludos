020fbd4946f0d8895541bf704755ec7d5973e95e
Luca Suriano <lucasuriano@live.it>
2014-09-15 23:40:32 +0200

    Formattato UtenteDAO

--- src/main/java/agroludos/integration/dao/db/UtenteDAO.java
+++ src/main/java/agroludos/integration/dao/db/UtenteDAO.java
@@ -5,12 +5,21 @@ import agroludos.exceptions.UserNotFoundException;
 import agroludos.to.UtenteTO;
 
 public interface UtenteDAO {
+
 	boolean crea(UtenteTO uto) throws DatabaseException;
+
 	UtenteTO autenticazione(UtenteTO uto) throws UserNotFoundException;
+
 	boolean esisteUsername(UtenteTO uto) throws DatabaseException;
+
 	boolean esisteEmail(UtenteTO uto) throws DatabaseException;
+
 	UtenteTO getByUsername(String username) throws DatabaseException;
+
 	UtenteTO getByID(Integer id) throws DatabaseException;
+
 	void setNomeRuolo(UtenteTO uto) throws DatabaseException;
+
 	void setNomeStatoUtente(UtenteTO uto) throws DatabaseException;
+
 }
