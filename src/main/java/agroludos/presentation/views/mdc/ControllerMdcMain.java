5bd4ef6f2ac6d88f71962315eb0bb897e69f8423
Luca Suriano <lucasuriano@live.it>
2014-09-16 21:00:51 +0200

    Controller MdC modificato

--- src/main/java/agroludos/presentation/views/mdc/ControllerMdcMain.java
+++ src/main/java/agroludos/presentation/views/mdc/ControllerMdcMain.java
@@ -1,25 +1,23 @@
 package agroludos.presentation.views.mdc;
 
-import java.net.URL;
-import java.util.ResourceBundle;
-
 import agroludos.presentation.req.AgroRequest;
 import agroludos.presentation.resp.AgroResponse;
 import agroludos.presentation.views.AgroludosController;
 import javafx.fxml.FXML;
-import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.GridPane;
 
-public class ControllerMdcMain extends AgroludosController implements Initializable{
+public class ControllerMdcMain extends AgroludosController{
 
 	@FXML private GridPane paneCompetizioni;
 	
 	@FXML private Button btnPaneComptizioni;
 	@FXML private Button btnNuovaCompetizione;
 
-	public void initialize(URL url, ResourceBundle resource) {
+	@Override
+	public void initializeView() {
+		// TODO Auto-generated method stub
 		
 	}
 
