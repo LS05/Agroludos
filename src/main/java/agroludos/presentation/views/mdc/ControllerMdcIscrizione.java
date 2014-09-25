292fa12ea67946a3d109d50882f38baf9fc05ca0
Francesco Zagaria <fr488@hotmail.com>
2014-09-25 10:47:42 +0200

    ggiunto command mostra iscrizione

--- src/main/java/agroludos/presentation/views/mdc/ControllerMdcIscrizione.java
+++ src/main/java/agroludos/presentation/views/mdc/ControllerMdcIscrizione.java
@@ -12,6 +12,7 @@ import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.GridPane;
 
 public class ControllerMdcIscrizione extends AgroludosController {
 
@@ -25,7 +26,7 @@ public class ControllerMdcIscrizione extends AgroludosController {
 	@FXML private Label lblIndirizzoIsc;
 	@FXML private Label lblDataSRCIsc;
 	@FXML private Label lblDataIsc;
-	
+
 	@FXML private Label lblCostolblOptional1;
 	@FXML private Label lblCostolblOptional2;
 	@FXML private Label lblCostolblOptional3;
@@ -39,7 +40,7 @@ public class ControllerMdcIscrizione extends AgroludosController {
 	@FXML private Button btnVisualizzaCertificato;
 	@FXML private Button btnAnnullaIscrizione;
 	@FXML private Button btnModificaOptionalIscrizione;
-	
+
 	@FXML private Label lblValuta1;
 	@FXML private Label lblValuta2;
 	@FXML private Label lblValuta3;
@@ -48,8 +49,13 @@ public class ControllerMdcIscrizione extends AgroludosController {
 	@FXML protected void btnAnnullaIscrizione(MouseEvent event) {}
 	@FXML protected void btnModificaOptionalIscrizione(MouseEvent event) {}
 
+	private String nameView;
+	@FXML private Label lblEliminaIscrizioneOk;
+	@FXML private GridPane paneIscrizione;
 	private IscrizioneTO iscto;
 	private List<OptionalTO> listOptionalScelti;
+	private AgroResponse risposta;
+	private AgroRequest richiesta;
 
 
 	@Override
@@ -68,7 +74,7 @@ public class ControllerMdcIscrizione extends AgroludosController {
 		this.lblValuta1.setVisible(false);
 		this.lblValuta2.setVisible(false);
 		this.lblValuta3.setVisible(false);
-		
+
 		this.lblNomeIsc.setText(this.iscto.getPartecipante().getNome());
 		this.lblCognomeIsc.setText(this.iscto.getPartecipante().getCognome());
 		this.lblEmailIsc.setText(this.iscto.getPartecipante().getEmail());
@@ -89,7 +95,7 @@ public class ControllerMdcIscrizione extends AgroludosController {
 				this.lblOptional1.setVisible(true);
 				this.lblTipoOptional1.setVisible(true);	
 				this.lblValuta1.setVisible(true);
-				
+
 				this.lblCostolblOptional1.setText(item.getCosto().toString());
 				this.lblOptional1.setText(item.getNome());
 				this.lblTipoOptional1.setText(item.getTipoOptional().getNome());
@@ -98,7 +104,7 @@ public class ControllerMdcIscrizione extends AgroludosController {
 				this.lblOptional2.setVisible(true);
 				this.lblTipoOptional2.setVisible(true);	
 				this.lblValuta2.setVisible(true);
-				
+
 				this.lblCostolblOptional2.setText(item.getCosto().toString());
 				this.lblOptional2.setText(item.getNome());
 				this.lblTipoOptional2.setText(item.getTipoOptional().getNome());
@@ -107,7 +113,7 @@ public class ControllerMdcIscrizione extends AgroludosController {
 				this.lblOptional2.setVisible(true);
 				this.lblTipoOptional2.setVisible(true);	
 				this.lblValuta3.setVisible(true);
-				
+
 				this.lblCostolblOptional2.setText(item.getCosto().toString());
 				this.lblOptional2.setText(item.getNome());
 				this.lblTipoOptional2.setText(item.getTipoOptional().getNome());
@@ -118,12 +124,22 @@ public class ControllerMdcIscrizione extends AgroludosController {
 
 	}
 
+	@FXML protected void btnAnnullaIscrizione(){
+		//TODO
+		System.out.println("Confermi? si...");
+		this.risposta = respFact.createResponse();
+		this.richiesta = this.getRichiesta(this.iscto, "eliminaIscrizione", this.nameView);
+		frontController.eseguiRichiesta(this.richiesta, this.risposta);
+	}
+
 	@Override
 	public void initializeView(String nameView) {
 		this.nameView = nameView;
+		this.lblEliminaIscrizioneOk.setVisible(false);
+		this.paneIscrizione.setDisable(true);
 
 	}
-	private String nameView;
+
 	@Override
 	protected String getNameView() {
 		return this.nameView;
@@ -133,11 +149,17 @@ public class ControllerMdcIscrizione extends AgroludosController {
 	protected void setNameView(String nameView) {
 		this.nameView = nameView;
 	}
-	
+
 	@Override
 	public void forward(AgroRequest request, AgroResponse response) {
-		// TODO Auto-generated method stub
+		if(request.getCommandName().equals("eliminaIscrizione")){
+			Object res = response.getRespData();
+			if(res instanceof IscrizioneTO){
+				this.lblEliminaIscrizioneOk.setVisible(true);
+				this.paneIscrizione.setDisable(false);
+			}
+		}
 
 	}
-	
+
 }
