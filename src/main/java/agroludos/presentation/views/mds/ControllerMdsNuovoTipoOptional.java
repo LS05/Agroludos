31d02fd75b278dab304799aa0243df1bd06fb184
Luca Suriano <lucasuriano@live.it>
2014-10-08 13:19:48 +0200

    Rimossa label per messaggio di successo. (Ci sarà un dialog)

--- src/main/java/agroludos/presentation/views/mds/ControllerMdsNuovoTipoOptional.java
+++ src/main/java/agroludos/presentation/views/mds/ControllerMdsNuovoTipoOptional.java
@@ -1,9 +1,6 @@
 package agroludos.presentation.views.mds;
 
-import javafx.event.EventHandler;
 import javafx.fxml.FXML;
-import javafx.scene.Scene;
-import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.input.MouseEvent;
 
@@ -16,7 +13,7 @@ import agroludos.to.TipoOptionalTO;
 public class ControllerMdsNuovoTipoOptional extends AgroludosController{
 	private String viewName;
 	private @FXML TextField txtNomeTipo;
-	private @FXML Label lblMsgNuovoTipo;
+	
 	private AgroRequest richiesta;
 	private AgroResponse risposta;
 
@@ -38,14 +35,6 @@ public class ControllerMdsNuovoTipoOptional extends AgroludosController{
 	@Override
 	protected void initializeView(String viewName) {
 		this.viewName = viewName;
-		Scene scene = nav.getStage(viewName).getScene();
-		scene.addEventFilter(MouseEvent.MOUSE_PRESSED, new EventHandler<MouseEvent>() {
-			@Override
-			public void handle(MouseEvent event) {
-				if(lblMsgNuovoTipo.isVisible())
-					lblMsgNuovoTipo.setVisible(false);
-			}
-		});
 	}
 
 	@Override
