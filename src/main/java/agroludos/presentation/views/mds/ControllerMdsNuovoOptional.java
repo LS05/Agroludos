aca80ef120e69114c60a15bc61851c04b279b69d
Luca Suriano <lucasuriano@live.it>
2014-10-08 19:12:46 +0200

    Aggiunti controlli per il prezzo di un optional.

--- src/main/java/agroludos/presentation/views/mds/ControllerMdsNuovoOptional.java
+++ src/main/java/agroludos/presentation/views/mds/ControllerMdsNuovoOptional.java
@@ -1,40 +1,57 @@
 package agroludos.presentation.views.mds;
 
+import java.math.BigDecimal;
+import java.net.URL;
+import java.text.DecimalFormat;
 import java.util.List;
+import java.util.ResourceBundle;
 
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.GridPane;
 
 import agroludos.presentation.req.AgroRequest;
 import agroludos.presentation.resp.AgroResponse;
 import agroludos.presentation.views.AgroludosController;
+import agroludos.presentation.views.components.numberspinner.NumberSpinner;
 import agroludos.to.AgroludosTO;
 import agroludos.to.OptionalTO;
 import agroludos.to.StatoOptionalTO;
+import agroludos.to.SuccessTO;
 import agroludos.to.TipoOptionalTO;
 
-public class ControllerMdsNuovoOptional extends AgroludosController{
+public class ControllerMdsNuovoOptional extends AgroludosController implements Initializable{
 	private String viewName;
 	private @FXML Label lblNomeTipoOpt;
 	private @FXML TextField txtNomeOptional;
-	private @FXML ComboBox<Double> cmbPrezzoOptional;
 	private @FXML ComboBox<String> cmbStatoOptional;
 	private @FXML TextArea txtAreaDescrizione;
+	private @FXML GridPane panePrezzoOptional;
+	private NumberSpinner costoOptional;
 	private AgroRequest richiesta;
 	private AgroResponse risposta;
 	private TipoOptionalTO tipoOpt;
 	private List<StatoOptionalTO> listStatiOpt;
 	private List<TipoOptionalTO> listTipiOpt;
-
+	private ResourceBundle res;
+	
+	@Override
+	public void initialize(URL location, ResourceBundle resources) {
+		this.res = resources;
+	}
+	
 	@Override
 	protected void initializeView(AgroludosTO mainTO) {
 		this.tipoOpt = (TipoOptionalTO)mainTO;
+		this.costoOptional = new NumberSpinner(BigDecimal.ZERO, new BigDecimal("0.05"), new DecimalFormat("#,##0.00"));
+		this.panePrezzoOptional.add(costoOptional, 1, 0);
 		this.lblNomeTipoOpt.setText(this.tipoOpt.getNome());
 
 		this.richiesta = this.getRichiesta("getAllStatoOptional", this.viewName);
@@ -59,9 +76,8 @@ public class ControllerMdsNuovoOptional extends AgroludosController{
 
 	@FXML protected void confermaNuovoOptional(MouseEvent event){
 		OptionalTO optional = toFact.createOptionalTO();
-
-		//		optional.setCosto(this.cmbPrezzoOptional.getValue());
-		optional.setCosto(20.6);
+		double costo = this.costoOptional.getNumber().doubleValue();
+		optional.setCosto(costo);
 		optional.setDescrizione(this.txtAreaDescrizione.getText());
 		optional.setNome(this.txtNomeOptional.getText());
 		
@@ -79,6 +95,9 @@ public class ControllerMdsNuovoOptional extends AgroludosController{
 		this.richiesta = this.getRichiesta(optional, "inserisciOptional", this.viewName);
 		this.risposta = respFact.createResponse();
 		frontController.eseguiRichiesta(this.richiesta, this.risposta);
+		SuccessTO msgNuovoOpt = toFact.createSuccessTO();
+		msgNuovoOpt.setMessagge(this.res.getString("key128"));
+		nav.setVista("successDialog", msgNuovoOpt);
 	}
 
 	@Override
