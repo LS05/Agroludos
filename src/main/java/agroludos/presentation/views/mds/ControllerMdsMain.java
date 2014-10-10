ee0fd9d42713e9ac9764c33485a000f64937e2ca
Luca Suriano <lucasuriano@live.it>
2014-10-10 20:16:52 +0200

    Inserimento tipo competizione aggiunto.

--- src/main/java/agroludos/presentation/views/mds/ControllerMdsMain.java
+++ src/main/java/agroludos/presentation/views/mds/ControllerMdsMain.java
@@ -83,6 +83,8 @@ public class ControllerMdsMain extends ControllerUtenti{
 
 	private ListaViewTipi listViewOpt;
 
+	private ListaViewTipi listViewComp;
+
 	private void setDxMdCColumn(Integer selected){
 		MdcModel selModel = tableManagerCompetizione.getItems().get(selected);
 		this.lblMdcNome.setText(selModel.getNome());
@@ -176,11 +178,11 @@ public class ControllerMdsMain extends ControllerUtenti{
 
 		this.tableCompetizioni = new TableCompetizioni();
 		this.paneTableCmp.getChildren().add(this.tableCompetizioni);
-		ListaViewTipi listViewComp = new ListaViewTipi(this.listTipiComp);
-		this.paneListaTipiComp.getChildren().add(listViewComp);
+		this.listViewComp = new ListaViewTipi(this.listTipiComp);
+		this.paneListaTipiComp.getChildren().add(this.listViewComp);
 
 
-		listViewComp.setOnMouseClicked(new EventHandler<MouseEvent>() {
+		this.listViewComp.setOnMouseClicked(new EventHandler<MouseEvent>() {
 
 			@Override
 			public void handle(MouseEvent event) {
@@ -275,8 +277,8 @@ public class ControllerMdsMain extends ControllerUtenti{
 		nav.setVista("modificaMDC", mdcto);
 	}
 
-	@FXML protected void btnNuovoTipoCompetizione(MouseEvent event) {
-
+	@FXML protected void nuovoTipoCompetizioneClicked(MouseEvent event) {
+		nav.setVista("nuovoTipoCpt");
 	}
 
 	@FXML protected void nuovoTipoOptionalClicked(MouseEvent event) {
@@ -425,6 +427,13 @@ public class ControllerMdsMain extends ControllerUtenti{
 			if(res instanceof OptionalTO){
 				OptionalTO optTO = (OptionalTO)res;
 				this.tableOptional.addItem(optTO);
+			}
+		} else if( commandName.equals( this.reqProperties.getProperty("inserisciTipoCompetizione") )){
+			Object res = response.getRespData();
+
+			if(res instanceof TipoCompetizioneTO){
+				TipoCompetizioneTO tipo = (TipoCompetizioneTO)res;
+				this.listViewComp.addItem(tipo);
 			}
 		}
 	}
