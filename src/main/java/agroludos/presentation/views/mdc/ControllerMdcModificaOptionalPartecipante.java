7dfb4542f121e6218d117712f6ef97f9fce61aa3
Francesco Zagaria <fr488@hotmail.com>
2014-10-22 19:57:48 +0200

    tolti warning

--- src/main/java/agroludos/presentation/views/mdc/ControllerMdcModificaOptionalPartecipante.java
+++ src/main/java/agroludos/presentation/views/mdc/ControllerMdcModificaOptionalPartecipante.java
@@ -1,32 +1,21 @@
 package agroludos.presentation.views.mdc;
 
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
-
 import javafx.beans.value.ChangeListener;
 import javafx.beans.value.ObservableValue;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
-import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
-import javafx.scene.control.ListView;
-import javafx.scene.control.TableCell;
-import javafx.scene.control.TableColumn;
-import javafx.scene.control.TableView;
-import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.GridPane;
-import javafx.util.Callback;
 import agroludos.presentation.req.AgroRequest;
 import agroludos.presentation.resp.AgroResponse;
 import agroludos.presentation.views.AgroludosController;
 import agroludos.presentation.views.components.table.TableOptional;
 import agroludos.presentation.views.components.tablemodel.OptModel;
-import agroludos.presentation.views.partecipante.DeleteTableCell;
 import agroludos.to.AgroludosTO;
 import agroludos.to.CompetizioneTO;
 import agroludos.to.IscrizioneTO;
@@ -63,7 +52,6 @@ public class ControllerMdcModificaOptionalPartecipante extends AgroludosControll
 	private AgroRequest richiesta;
 	private List<TipiAgroludosTO> listTipiOpt;
 	private int passoCorrente;
-	private List<OptionalTO> listOptScelti;
 	private ObservableList<OptModel> optSceltiData;
 	private double totale;
 
@@ -72,7 +60,6 @@ public class ControllerMdcModificaOptionalPartecipante extends AgroludosControll
 		this.mainIsc = (IscrizioneTO) mainTO;
 		this.mainCmp = this.mainIsc.getCompetizione();
 
-		this.listOptScelti = new ArrayList<OptionalTO>();
 		this.optSceltiData = FXCollections.observableArrayList();
 
 		this.risposta = respFact.createResponse();
