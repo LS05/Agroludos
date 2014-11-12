8beabbf56816e97979c880f19037420f2b532d23
Luca Suriano <lucasuriano@live.it>
2014-11-12 11:17:45 +0100

    Rimosso metodo inutilizzato

--- src/main/java/agroludos/business/as/gestoremail/LEmail.java
+++ src/main/java/agroludos/business/as/gestoremail/LEmail.java
@@ -5,9 +5,6 @@ import agroludos.exceptions.DatabaseException;
 import agroludos.to.EmailTO;
 
 public interface LEmail extends AgroludosService{
-	
-	//TODO Da rivedere
-	boolean sendEmail(EmailTO email) throws DatabaseException;
 
 	EmailTO inviaMail(EmailTO mail) throws DatabaseException;
 
