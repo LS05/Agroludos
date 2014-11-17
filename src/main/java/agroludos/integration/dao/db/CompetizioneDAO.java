41ca95e9b756a495fcdfe3ebf2519ddd8c9ea5c8
Luca Suriano <lucasuriano@live.it>
2014-11-17 01:23:10 +0100

    Aggiunti JavaDoc

--- src/main/java/agroludos/integration/dao/db/CompetizioneDAO.java
+++ src/main/java/agroludos/integration/dao/db/CompetizioneDAO.java
@@ -10,8 +10,8 @@ import agroludos.to.TipoCompetizioneTO;
 /** 
  * Data Access Object per tutte le operazioni CRUD per quanto riguarda le Competizioni.
  * Sono presenti tutti i metodi di scrittura e aggiornamento così come anche di lettura di un tipo particolare di
- * competizioni (attive, aperte, gestite da un particolare manager o di un tipo particolare).
- * 
+ * competizioni (attive, gestite da un particolare manager o di un tipo particolare).
+ * Per competizioni attive s'intende una competizione con lo stato: aperta alle iscrizioni, chiusa o in corso.
  * @author Luca Suriano
  * @author Francesco Zagaria
  *
@@ -23,30 +23,62 @@ public interface CompetizioneDAO extends DAO<CompetizioneTO>{
 	 * 
 	 * @param cmpTO La competizione da annullare
 	 * @return La competizione appena annullata.
-	 * @throws DatabaseException
+	 * @throws DatabaseException Sollevata in caso di problemi di accesso al database
 	 */
 	CompetizioneTO annullaCompetizione(CompetizioneTO cmpTO) throws DatabaseException;
 
 	/**
-	 * Restituisce una lista di competizioni in base al tipo specificato come parametro
+	 * Restituisce la lista delle competizioni attive gestite dal manager di competizione
 	 * 
-	 * @param tipo Tipo utilizzato per restituire le competizioni
-	 * @return Lista di competizioni del tipo specificato
-	 * 
-	 * @throws DatabaseException
+	 * @param mdcTO Manager di competizione da utilizzare per la lettura
+	 * @return La lista delle competizioni gestite dal manager di competizione
+	 * @throws DatabaseException Sollevata in caso di problemi di accesso al database
 	 */
-	List<CompetizioneTO> readByTipo(TipoCompetizioneTO tipo) throws DatabaseException;
+	List<CompetizioneTO> readAttiveByMdc(ManagerDiCompetizioneTO mdcTO) throws DatabaseException;
 
-	List<CompetizioneTO> readAttiveByMdc(ManagerDiCompetizioneTO mdc) throws DatabaseException;
-
+	/**
+	 * Restituisce una competizione in base al suo id
+	 * 
+	 * @param id L'id della competizione
+	 * @return La competizione associata all'id specificato
+	 * @throws DatabaseException Sollevata in caso di problemi di accesso al database
+	 */
 	CompetizioneTO readById(Integer id) throws DatabaseException;
 
+	/**
+	 * Restituisce la lista delle competizioni attive
+	 * 
+	 * @return Lista delle competizioni attive
+	 * @throws DatabaseException Sollevata in caso di problemi di accesso al database
+	 */
 	List<CompetizioneTO> readCompetizioniAttive() throws DatabaseException;
 
+	/**
+	 * Restituisce la lista delle competizioni aperte
+	 * 
+	 * @return Lista delle competizioni aperte
+	 * @throws DatabaseException Sollevata in caso di problemi di accesso al database
+	 */
 	List<CompetizioneTO> readCompetizioniAperte() throws DatabaseException;
 
+	/**
+	 * Restituisce la lista di tutte le competizioni gestite dal manager di competizione specificato 
+	 * (sia attive che non).
+	 * 
+	 * @param mdcTO Il manager di competizione usato per la lettura
+	 * @return La lista delle competizioni attive
+	 * @throws DatabaseException Sollevata in caso di problemi di accesso al database
+	 */
 	List<CompetizioneTO> readByMdc(ManagerDiCompetizioneTO mdcTO)throws DatabaseException;
 
+	/**
+	 * Restituisce una lista di competizioni in base al tipo specificato come parametro
+	 * (sia attive che non).
+	 * 
+	 * @param tcmTO Tipo Competizione da usare per la lettura
+	 * @return Lista di competizioni del tipo specificato
+	 * @throws DatabaseException Sollevata in caso di problemi di accesso al database
+	 */
 	List<CompetizioneTO> getCompetizioniByTipo(TipoCompetizioneTO tcmTO) throws DatabaseException;
 	
 }
