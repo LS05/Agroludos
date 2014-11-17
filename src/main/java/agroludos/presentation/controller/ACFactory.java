fa447700f4783e753a19f1c9b3f1a167052253ce
Luca Suriano <lucasuriano@live.it>
2014-11-17 01:52:14 +0100

    Aggiunti JavaDoc

--- src/main/java/agroludos/presentation/controller/ACFactory.java
+++ src/main/java/agroludos/presentation/controller/ACFactory.java
@@ -1,5 +1,19 @@
 package agroludos.presentation.controller;
 
+/**
+ * L'interfaccia rappresenta il Factory per l'application controller.
+ * 
+ * @author Luca Suriano
+ * @author Francesco Zagaria
+ *
+ */
 public interface ACFactory {
+	
+	/**
+	 * Il metodo restituisce un'istanza di una classe che implementa l'interfaccia
+	 * {@link ApplicationController}
+	 * 
+	 * @return Classe che implementa l'interfaccia {@link ApplicationController}
+	 */
 	ApplicationController getAC();
 }
--- src/main/java/agroludos/presentation/controller/ApplicationController.java
+++ src/main/java/agroludos/presentation/controller/ApplicationController.java
@@ -12,8 +12,21 @@ import agroludos.presentation.resph.AgroResponseContext;
  */
 public interface ApplicationController {
 
+	/**
+	 * Il metodo si occupa di gestire la richiesta espressa come parametro.<br>
+	 * In base alla sua esecuzione il risultato è restituito tramite un'istanza di {@link AgroResponseContext}
+	 * 
+	 * @param request Richiesta da utilizzare per l'esecuzione di un particolare servizio
+	 * @return
+	 */
 	public AgroResponseContext gestisciRichiesta(AgroRequestContext request);
 
+	/**
+	 * Il metodo si occupa di effettuare il dispatching.
+	 * 
+	 * @param requestContext Richiesta del servizio da eseguire
+	 * @param responseContext Riposta ottenuta dalla sua esecuzione
+	 */
 	public void gestisciRisposta(AgroRequestContext requestContext, AgroResponseContext responseContext);
 
 }
